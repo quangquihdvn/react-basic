e95880932860238031351017f0ed98c6fb9880de
Bùi Quang Quí <qui.bq@3si.vn>
2024-02-26 13:48:50 +0700

    done clip 40

--- src/containers/System/ModalUser.js
+++ src/containers/System/ModalUser.js
@@ -26,29 +26,46 @@ class ModalUser extends Component {
             <Modal isOpen={this.props.isOpen}
                 toggle={() => this.toggle()}
                 size='lg'
-                centered
+                className='modal-user-container'
             >
                 <ModalHeader toggle={() => this.toggle()}>Create a new user</ModalHeader>
                 <ModalBody>
-                    <div className='container'>
-                        <div className='row'>
-                            <div className='col-6 form-group'>
-                                <label>Email</label>
-                                <input type='text' />
-                            </div>
-                            <div className='col-6 form-group'>
-                                <label>Password</label>
-                                <input type='password' />
-                            </div>
+                    <div className='modal-user-body'>
+                        <div className='input-container'>
+                            <label>Email</label>
+                            <input type='text' />
+                        </div>
+                        <div className='input-container'>
+                            <label>Password</label>
+                            <input type='password' />
+                        </div>
+                        <div className='input-container'>
+                            <label>Firstname</label>
+                            <input type='text' />
+                        </div>
+                        <div className='input-container'>
+                            <label>Lastname</label>
+                            <input type='text' />
+                        </div>
+                        <div className='input-container max-width-input'>
+                            <label>Address</label>
+                            <input type='text' />
                         </div>
                     </div>
+                    
                 </ModalBody>
                 <ModalFooter>
-                    <Button color="primary" onClick={() => this.toggle()}>
-                        Do Something
+                    <Button color="primary" 
+                    onClick={() => this.toggle()}
+                    className='px-3'
+                    >
+                        Save changes
                     </Button>{' '}
-                    <Button color="secondary" onClick={() => this.toggle()}>
-                        Cancel
+                    <Button color="secondary" 
+                    onClick={() => this.toggle()}
+                    className='px-3'
+                    >
+                        Close
                     </Button>
                 </ModalFooter>
             </Modal>
