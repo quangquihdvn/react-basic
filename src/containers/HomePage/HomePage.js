44ef3072fd3c6fb1fab0e3cdd67ef46de5f62d5c
Bùi Quang Quí <qui.bq@3si.vn>
2024-02-28 14:48:41 +0700

    done video 50

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -2,16 +2,43 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import HomeHeader from './HomeHeader';
 import Specialty from './Section/Specialty';
+import Medicalfacility from './Section/Medicalfacility';
+import OutStandingDoctor from './Section/OutStandingDoctor';
+import HandBook from './Section/HandBook';
+import './HomePage.scss';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 class HomePage extends Component {
 
     render() {
 
+        let settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+        };
+
         return (
             <div>
                 <HomeHeader />
-                <Specialty />
-                <div style={{ height: '300px' }}></div>
+                <Specialty 
+                settings = {settings}
+                />
+
+                <Medicalfacility 
+                settings = {settings}/>
+
+                <OutStandingDoctor
+                settings = {settings}
+                />
+
+                <HandBook
+                settings = {settings}
+                />
             </div>
         );
     }
