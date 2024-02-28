8a46f151e1f03cfb4bd555849215d613ffc88b20
Bùi Quang Quí <qui.bq@3si.vn>
2024-02-28 16:59:52 +0700

    done video 51

---
+++ src/containers/HomePage/HomeFooter.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+class HomeFooter extends Component {
+
+    render() {
+        
+        return (
+            <div className='home-footer'>
+                <p>
+                    &copy; 2024 Training reactjs by Bui Quang Qui. 
+                    <a 
+                    target="_blank"
+                    href='https://www.youtube.com/watch?v=147SkAVXEqM&list=PLncHg6Kn2JT7q7xHz8Ns3vP0ETxMJ78ts&index=28'>More infomation, please visit my youtube channel. &#8594; Click here &#8592;</a>
+                </p>
+            </div>
+        );
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        isLoggedIn: state.user.isLoggedIn,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeFooter);
---
+++ src/containers/HomePage/Section/About.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+class About extends Component {
+
+    render() {
+        
+        return (
+            <div className='section-share section-about'>
+                <div className='section-about-header'>
+                    Truyền thông nói gì về React
+                </div>
+                <div className='section-about-content'>
+                    <div className='content-left'>
+                        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/147SkAVXEqM?list=PLncHg6Kn2JT7q7xHz8Ns3vP0ETxMJ78ts" 
+                        title="#51 Kết Thúc Design Giao Diện Clone BookingCare.vn 4 | React.JS Cho Người Mới Bắt Đầu" 
+                        frameborder="0" 
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                    <div className='content-right'>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                </div>
+                </div>
+                
+            </div>
+        );
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        isLoggedIn: state.user.isLoggedIn,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(About);
