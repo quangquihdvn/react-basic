b946814f4528d3ebf88d16165751e2c6679b1036
Bùi Quang Quí <qui.bq@3si.vn>
2024-03-05 14:20:45 +0700

    done video 66

--- src/containers/System/Admin/ManageDoctor.js
+++ src/containers/System/Admin/ManageDoctor.js
@@ -8,25 +8,56 @@ import MdEditor from 'react-markdown-editor-lite';
 // import style manually
 import 'react-markdown-editor-lite/lib/index.css';
 
+import './ManageDoctor.scss';
+import Select from 'react-select';
+
+
 // Register plugins if required
 // MdEditor.use(YOUR_PLUGINS_HERE);
 
 // Initialize a markdown parser
 const mdParser = new MarkdownIt(/* Markdown-it options */);
 
-
+const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+];
 
 class ManageDoctor extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            contentMarkdown: '',
+            contentHTML: '',
+            selectedOption: null,
+            description: ''
         }
     }
 
     // Finish!
-    handleEditorChange({ html, text }) {
+    handleEditorChange = ({ html, text }) => {
+        this.setState({
+            contentMarkdown: text,
+            contentHTML: html
+        })
         console.log('handleEditorChange', html, text);
+    }
+
+    handleSaveContentMarkdown = () => {
+        console.log('=============', this.state);
+    }
+
+    handleChange = (selectedOption) => {
+        this.setState({ selectedOption }
+        );
+    };
+
+    handleOnChangeDesc = (event) => {
+        this.setState({
+            description: event.target.value
+        })
     }
 
     /**Life cyle
@@ -39,12 +70,38 @@ class ManageDoctor extends Component {
         return (
             <div className='manage-doctor-container'>
                 <div className='manage-doctor-title'>Tạo thêm thông tin doctors</div>
+
+                <div className='more-info'>
+                    <div className='content-left form-group'>
+                        <label>Chọn bác sĩ</label>
+                        <Select
+                            value={this.state.selectedOption}
+                            onChange={this.handleChange}
+                            options={options}
+                        />
+
+                    </div>
+                    <div className='content-right'>
+                        <label>Thông tin giới thiệu:</label>
+                        <textarea className='form-control' row="4"
+                            onChange={(event) => this.handleOnChangeDesc(event)}
+                            value={this.state.description}
+                        >
+                            dưqdwqdwqdwqd
+                        </textarea>
+                    </div>
+
+                </div>
                 <div className='manage-doctor-editor'>
                     <MdEditor style={{ height: '500px' }}
                         renderHTML={text => mdParser.render(text)}
-                        onChange={() => this.handleEditorChange} />
+                        onChange={this.handleEditorChange} />
                 </div>
-
+                <button className='save-content-doctor'
+                    onClick={() => this.handleSaveContentMarkdown()}
+                >
+                    Lưu thông tin
+                </button>
             </div>
         );
     }
